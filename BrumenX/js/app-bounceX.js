9e4fd17b59f49956edbfca8c85241fc51d5b87ca
Ljudje-si <84963045+Ljudje-si@users.noreply.github.com>
2021-06-18 15:26:05 +0200

    Update app-bounceX.js

--- BrumenX/js/app-bounceX.js
+++ BrumenX/js/app-bounceX.js
@@ -25,49 +25,116 @@ function setup() {
 
 }
 
+function brumenLogo() {
+
+  noStroke()
+
+  circle(2 + bounceX, 2 + bounceY, 4)
+  circle(7 + bounceX, 2 + bounceY, 4)
+  circle(12 + bounceX, 2 + bounceY, 4)
+  circle(17 + bounceX, 2 + bounceY, 4)
+
+  circle(2 + bounceX, 7 + bounceY, 4)
+  circle(7 + bounceX, 7 + bounceY, 4)
+  circle(12 + bounceX, 7 + bounceY, 4)
+  circle(17 + bounceX, 7 + bounceY, 4)
+
+  circle(2 + bounceX, 12 + bounceY, 4)
+  circle(7 + bounceX, 12 + bounceY, 4)
+  circle(12 + bounceX, 12 + bounceY, 4)
+  circle(17 + bounceX, 12 + bounceY, 4)
+  circle(22 + bounceX, 12 + bounceY, 4)
+  circle(27 + bounceX, 12 + bounceY, 4)
+  circle(32 + bounceX, 12 + bounceY, 4)
+  circle(37 + bounceX, 12 + bounceY, 4)
+
+  circle(2 + bounceX, 17 + bounceY, 4)
+  circle(7 + bounceX, 17 + bounceY, 4)
+  circle(12 + bounceX, 17 + bounceY, 4)
+  circle(17 + bounceX, 17 + bounceY, 4)
+  circle(32 + bounceX, 17 + bounceY, 4)
+  circle(37 + bounceX, 17 + bounceY, 4)
+
+  circle(2 + bounceX, 22 + bounceY, 4)
+  circle(7 + bounceX, 22 + bounceY, 4)
+  circle(12 + bounceX, 22 + bounceY, 4)
+  circle(17 + bounceX, 22 + bounceY, 4)
+  circle(32 + bounceX, 22 + bounceY, 4)
+  circle(37 + bounceX, 22 + bounceY, 4)
+
+  circle(2 + bounceX, 27 + bounceY, 4)
+  circle(7 + bounceX, 27 + bounceY, 4)
+  circle(12 + bounceX, 27 + bounceY, 4)
+  circle(17 + bounceX, 27 + bounceY, 4)
+  circle(22 + bounceX, 27 + bounceY, 4)
+  circle(27 + bounceX, 27 + bounceY, 4)
+  circle(32 + bounceX, 27 + bounceY, 4)
+  circle(37 + bounceX, 27 + bounceY, 4)
+
+  circle(2 + bounceX, 32 + bounceY, 4)
+  circle(7 + bounceX, 32 + bounceY, 4)
+  circle(12 + bounceX, 32 + bounceY, 4)
+  circle(17 + bounceX, 32 + bounceY, 4)
+  circle(22 + bounceX, 32 + bounceY, 4)
+  circle(27 + bounceX, 32 + bounceY, 4)
+  circle(32 + bounceX, 32 + bounceY, 4)
+  circle(37 + bounceX, 32 + bounceY, 4)
+
+  circle(2 + bounceX, 37 + bounceY, 4)
+  circle(7 + bounceX, 37 + bounceY, 4)
+  circle(12 + bounceX, 37 + bounceY, 4)
+  circle(17 + bounceX, 37 + bounceY, 4)
+  circle(22 + bounceX, 37 + bounceY, 4)
+  circle(27 + bounceX, 37 + bounceY, 4)
+  circle(32 + bounceX, 37 + bounceY, 4)
+  circle(37 + bounceX, 37 + bounceY, 4)
+}
+
 // draws
 
 function draw() {
 
-  
+
   strokeCap(SQUARE);
   strokeWeight(bounceXWeight);
 
   fill(random(255), random(255), random(255));
-  noStroke();
   stroke(random(255), random(255), random(255))
-  line(bounceX, bounceY, bounceX - (bounceXSize / 2), bounceY + (bounceXSize / 2))
+  /* line(bounceX, bounceY, bounceX - (bounceXSize / 2), bounceY + (bounceXSize / 2))
   line(bounceX, bounceY, bounceX + (bounceXSize / 2), bounceY - (bounceXSize / 2))
   line(bounceX, bounceY, bounceX - (bounceXSize / 2), bounceY - (bounceXSize / 2))
-  line(bounceX, bounceY, bounceX + (bounceXSize / 2), bounceY + (bounceXSize / 2))
+  line(bounceX, bounceY, bounceX + (bounceXSize / 2), bounceY + (bounceXSize / 2)) */
+
+  brumenLogo()
+
 
   bounceX = bounceX + bounceSpeedX;
   bounceY = bounceY + bounceSpeedY;
 
-  if (bounceX > canvasWidth - bounceXSize) {
-    bounceSpeedX = -6;
-    bounceXSize = random(100)
+  if (bounceX > canvasWidth - bounceXSize - 18) {
+    bounceSpeedX = -5;
+    //bounceXSize = random(100)
     bounceXWeight = random(50)
   }
 
-  if (bounceX < bounceXSize) {
-    bounceSpeedX = 6;
+  if (bounceX < bounceXSize - 18) {
+    bounceSpeedX = 5;
     color(0, 0, 0, 0)
-    bounceXSize = random(100)
+    //bounceXSize = random(100)
     bounceXWeight = random(50)
   }
 
-  if (bounceY > canvasHeight - bounceXSize) {
+  if (bounceY > canvasHeight - bounceXSize - 18) {
     bounceSpeedY = -5;
     color(0, 0, 0, 0)
-    bounceXSize = random(100)
+    //bounceXSize = random(100)
     bounceXWeight = random(50)
   }
 
-  if (bounceY < bounceXSize) {
+  if (bounceY < bounceXSize - 18) {
     bounceSpeedY = 5;
     color(0, 0, 0, 0)
-    bounceXSize = random(100)
+    //bounceXSize = random(100)
     bounceXWeight = random(50)
   }
 
